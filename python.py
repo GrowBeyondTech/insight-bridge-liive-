d5ce7759d65b9ac5d0baba4bae53a6cc6253c8f5
yashu_wini <yashaswinikcb@gmail.com>
2025-09-22 15:44:15 +0530

    Removed unwanted file(s)

--- python.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("companies.db")
-cur = conn.cursor()
-cur.execute("SELECT id, name, industry FROM companies;")
-rows = cur.fetchall()
-print(rows)
-conn.close()
